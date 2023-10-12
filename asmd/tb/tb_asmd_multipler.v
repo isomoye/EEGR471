c2d9c20927e95be7ae876f9ddcdf00a6a681cbff
omer <omoll1@morgan.edu>
2023-10-12 19:22:39 -0400

    lab 3

--- asmd/tb/tb_asmd_multipler.v
+++ asmd/tb/tb_asmd_multipler.v
@@ -8,10 +8,13 @@ module asmd_multiplier_tb;
   wire [2*word_length-1:0] product;
   wire  ready;
   reg [word_length-1:0] word0;
-  wire  word1;
+  reg [word_length-1:0]  word1;
   reg  start;
   reg  clk=0;
-  wire  reset;
+  reg  reset;
+
+  //model signal
+  reg [2*word_length-1:0] model_product
 
 //asmd multiplier design-under-test (DUT)
   asmd_multiplier # (
@@ -34,21 +37,39 @@ always #5  clk = ! clk ;
 initial begin
     $dumpfile("asmd_multiplier.vcd");
     $dumpvars(0, asmd_multiplier_tb);
+    #100  //wait 100 time units
+    
     //assert reset
+    reset = 1'b1;
+    #100 //wait 100 time units
 
     //check product and ready value
+      if(product != 8'b0)begin
+        $error("Product is not zero");
+      end
 
-
+      if(ready != 1'b1)begin
+        $error("Ready not asserted");
+      end
     //deassert reset
+    reset = 1'b0;
 
     //set word0 and word1
-
+    word0 = 4'b0100;
+    word1 = 4'b0101;
     //set start
-
+    start = 1'b1;
+    
+    //model_product = word0 * word1;
+    product = word0 * word1;
     //wait for ready
-
+    wait(ready);
 
     //check product value
+    if(model_product != product) begin
+      $error("Value of the product is not correct");
+    end
+
     $finish();
 
 end
