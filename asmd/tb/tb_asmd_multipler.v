3729762df9363c7829d53e2283d9d4ae85278b45
omer <omoll1@morgan.edu>
2023-10-15 23:20:23 -0400

    lab 3

--- asmd/tb/tb_asmd_multipler.v
+++ asmd/tb/tb_asmd_multipler.v
@@ -1,21 +1,20 @@
+//Engineers : Omer Olloumou, Brooke Price, Ifeanyichukwo Iwobi
 
 module asmd_multiplier_tb;
 
   // Parameters
-  localparam word_length = 0;
+  localparam word_length = 4 ;
 
   //Ports
   wire [2*word_length-1:0] product;
   wire  ready;
   reg [word_length-1:0] word0;
-  reg [word_length-1:0]  word1;
+  reg [word_length-1:0] word1;
   reg  start;
   reg  clk=0;
   reg  reset;
 
-  //model signal
-  reg [2*word_length-1:0] model_product
-
+  reg [2*word_length-1:0]  model_product;
 //asmd multiplier design-under-test (DUT)
   asmd_multiplier # (
     .word_length(word_length)
@@ -31,44 +30,47 @@ module asmd_multiplier_tb;
   );
 
 
-//clock
-always #5  clk = ! clk ;
+//clock Generation
+always #5  clk = !clk ;
 
 initial begin
     $dumpfile("asmd_multiplier.vcd");
     $dumpvars(0, asmd_multiplier_tb);
-    #100  //wait 100 time units
+    #10  //wait 100 time units
     
-    //assert reset
+  //assert reset
     reset = 1'b1;
-    #100 //wait 100 time units
-
+    #10 //wait 100 time units
+    
     //check product and ready value
       if(product != 8'b0)begin
         $error("Product is not zero");
       end
-
-      if(ready != 1'b1)begin
+      if(ready != 1'b0)begin
         $error("Ready not asserted");
       end
     //deassert reset
     reset = 1'b0;
-
+    
     //set word0 and word1
-    word0 = 4'b0100;
-    word1 = 4'b0101;
+    word0 = 4'b0100; //4
+    word1 = 4'b0101; //5
+    
     //set start
     start = 1'b1;
     
-    //model_product = word0 * word1;
-    product = word0 * word1;
+    model_product = word0*word1;
+    
+    
     //wait for ready
     wait(ready);
 
     //check product value
-    if(model_product != product) begin
-      $error("Value of the product is not correct");
+    if(product != model_product)begin
+      $error("conent of  model_product is %d", model_product); 
+      
     end
+    
 
     $finish();
 
