1fb377ea69e8bcc26ff81c6cfa3a4000fbac1f1b
ismoo4 <isaiah085@gmail.com>
2024-09-19 16:06:04 -0400

    Lab 3

--- asmd/tb/tb_asmd_multipler.v
+++ asmd/tb/tb_asmd_multipler.v
@@ -2,16 +2,17 @@
 module asmd_multiplier_tb;
 
   // Parameters
-  localparam  word_length = 0;
+  localparam  word_length = 4;
 
   //Ports
   wire [2*word_length-1:0] product;
   wire  ready;
   reg [word_length-1:0] word0;
-  wire  word1;
+  reg [word_length-1:0]  word1;
   reg  start;
-  wire  clk;
-  wire  reset;
+  reg  clk;
+  reg  reset;
+
 
   asmd_multiplier # (
     .word_length(word_length)
@@ -26,6 +27,28 @@ module asmd_multiplier_tb;
     .reset(reset)
   );
 
-//always #5  clk = ! clk ;
+always #5  clk = ! clk ;
+initial begin
+  $dumpfile("asmd_multiplier.vcd");
+  $dumpvars(0,asmd_multiplier_tb);
+end
+initial begin
+  clk = 0;
+  reset = 1;
+  start = 0;
+  word0 = 4'b0000;
+  word1 = 4'b0000;
+  #100;
+  reset = 0;
+  word0 = 4'b0011;
+  word1 = 4'b0010;
+  start = 1;
+  #65;
+  start = 0;
+  wait(ready == 1);
+  $display("3*2 = %d, product = %b", word0 * word1,product);
+  #10;
+  $finish;
+end
 
 endmodule
