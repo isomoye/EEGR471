58eab0b4bfca1eaba9d1032f117c5d0a6a9a2fec
Oluwaseyifunmi Williams <ol.williams@capc-cl1-1.msucap.com>
2024-10-01 12:49:44 -0400

    final revision -lab3

--- asmd/tb/tb_asmd_multiplier.v
+++ asmd/tb/tb_asmd_multiplier.v
@@ -1,125 +1,121 @@
+ 
+
 module asmd_multiplier_tb;
 
-  // Parameters
-  localparam word_length = 4; //confirm word length by inspection
+// Parameters
+localparam word_length = 4; //confirm word length by inspection
 
-  // Ports (inputs and outputs)
-  wire [2*word_length-1:0] product;
-  wire ready;
-  reg [word_length-1:0] word0;
-  reg [word_length-1:0] word1;
-  reg start;
-  reg clk = 0;
-  reg reset;
-  reg [2*word_length-1:0] model_product;
+// Ports (inputs and outputs)
+wire [2*word_length-1:0] product;
+wire ready;
+reg [word_length-1:0] word0;
+reg [word_length-1:0] word1;
+reg start;
+reg clk = 0;
+reg reset;
+reg [2*word_length-1:0] model_product;
 
-  // Additional ports to link the netlist
-  wire ready_netlist;
-  wire [2*word_length-1:0] product_netlist;
 
-  // ASMD multiplier design-under-test (DUT)
-  asmd_multiplier #(
-    .word_length(word_length)
-  )
+// ASMD multiplier design-under-test (DUT)
+asmd_multiplier #(
+  .word_length(word_length)
+)
+
+asmd_multiplier_inst (
+  .product(product),
+  .ready(ready),
+  .word0(word0),
+  .word1(word1),
+  .start(start),
+  .clk(clk),
+  .reset(reset)
+);
+
+
+always @ (posedge clk) begin
+  if (!reset) begin
+    @(posedge clk)
+      if (ready!=1'b1)
+      $error("ready is not asserted when reset is deasserted");
+  end
+  else begin
+    if (product != 0)
+      $error("product is not set to 0");
+  end
+end
+
+initial begin
+  if($bits(asmd_multiplier_inst.datapath_module0.product) != $bits(product)) begin
+    $error("TP002: product bitwidth incorrect, %d",$bits(asmd_multiplier_inst.datapath_module0.product));
+  end
+  else begin
+    $display("TP002: pass");
+  end
+end
+
+
+// Monitor Ready and Product signals for both RTL and Netlist
+
+
+
+always #5 clk = !clk; //init clock
+
+// Testbench logic
+initial begin
+  $dumpfile("asmd_multiplier.vcd");
+  $dumpvars(0, asmd_multiplier_tb);
+
+  // Initialize inputs ---TP001: inspect inputs at initialization
+  //                   ---TP002: check that word length is as specified by visual inspection bin val
+  start = 1'b0;
+  reset = 1'b1;
+  word0 = 4'h4;   // Set word0 = 4
+  word1 = 4'h5;   // Set word1 = 5
+  model_product = word0 * word1; // Expected product (4 * 5 = 20 decimal)
+
+// Clock Generation    ---TP003: setup clock, visual inspect accurate period
+
+
+  // Wait for some cycles with reset asserted
+  #20;
+  reset = 1'b0;  // Deassert reset after initialization and watch for ready bit & word inputs
+  #10;
+
+  // Start the multiplication
+  start = 1'b1;
+  #10;
+  start = 1'b0;
   
-  
-  asmd_multiplier_inst (
-    .product(product),
-    .ready(ready),
-    .word0(word0),
-    .word1(word1),
-    .start(start),
-    .clk(clk),
-    .reset(reset)
-  );
+  // Wait for ready to become low and then high (to ensure calculation completes)
+  wait (!ready);  // Wait until ready goes low (indicating processing started)
+  wait (ready);   // Wait until ready goes high (indicating processing complete)
+  #10;            // Add a small delay to ensure product has time to settle
 
-  // ASMD multiplier netlist design-under-test (DUT)
-  asmd_multiplier_netlist
-  asmd_multiplier_netlist_inst (
-    .product(product_netlist),
-    .ready(ready_netlist),
-    .word0(word0),    // Connect word0 from the testbench
-    .word1(word1),    // Connect word1 from the testbench
-    .start(start),    // Connect start from the testbench
-    .clk(clk),        // Connect clk from the testbench
-    .reset(reset)     // Connect reset from the testbench
-  );
+  reset = 1'b1;
+  #10
+  reset = 1'b0;
+  #20
+  start = 1'b0;
+  word0 = 15;   // Set word0 = 15
+  word1 = 15;   // Set word1 = 15
+  start = 1'b1;
+  #10;
+  start = 1'b0;
+  model_product = word0 * word1;
+  wait (!ready);  // Wait until ready goes low (indicating processing started)
+  wait (ready);   // Wait until ready goes high (indicating processing complete)
 
 
-
-  // Monitor Ready and Product signals for both RTL and Netlist
-
-
-  // Confirm that netlist product and RTL product output are equivalent.
-  always @(*) begin
-    if (product != product_netlist) begin
-      $error("Netlist / RTL product mismatch at time %0t", $time);
-    end
-    if (ready != ready_netlist) begin
-      $error("Netlist / RTL ready mismatch at time %0t", $time);
-    end
+  // Check product value after ready is high
+  if (model_product != product) begin
+    $error("TP001: Model product (%d) not equal to product (%d) at time %0t when ready is 1", model_product, product, $time);
+  end else begin
+    $display("TP001: Success! product is %d at time %t", product, $time);
   end
 
-  always #5 clk = !clk; //init clock
 
-  // Testbench logic
-  initial begin
-    $dumpfile("asmd_multiplier.vcd");
-    $dumpvars(0, asmd_multiplier_tb);
-
-    // Initialize inputs ---TP001: inspect inputs at initialization
-    //                   ---TP002: check that word length is as specified by visual inspection bin val
-    start = 1'b0;
-    reset = 1'b1;
-    word0 = 4'h4;   // Set word0 = 4
-    word1 = 4'h5;   // Set word1 = 5
-    model_product = word0 * word1; // Expected product (4 * 5 = 20 decimal)
-
-  // Clock Generation    ---TP003: setup clock, visual inspect accurate period
-  
-
-    // Wait for some cycles with reset asserted
-    #20;
-    reset = 1'b0;  // Deassert reset after initialization and watch for ready bit & word inputs
-    #10;
-
-    // Start the multiplication
-    start = 1'b1;
-    #10;
-    start = 1'b0;
-    
-    // Wait for ready to become low and then high (to ensure calculation completes)
-    wait (!ready);  // Wait until ready goes low (indicating processing started)
-    wait (ready);   // Wait until ready goes high (indicating processing complete)
-    #10;            // Add a small delay to ensure product has time to settle
-
-    reset = 1'b1;
-    #10
-    reset = 1'b0;
-    #20
-    start = 1'b0;
-    word0 = 15;   // Set word0 = 15
-    word1 = 15;   // Set word1 = 15
-    start = 1'b1;
-    #10;
-    start = 1'b0;
-    model_product = word0 * word1;
-    wait (!ready);  // Wait until ready goes low (indicating processing started)
-    wait (ready);   // Wait until ready goes high (indicating processing complete)
-
-
-    // Check product value after ready is high
-    if (model_product != product) begin
-      $error("Model product (%d) not equalw to RTL product (%d) at time %0t", model_product, product, $time);
-    end else begin
-      $display("Success! RTL Product is %d, which matches model product at time %0t", product, $time);
-    end
-
-    // Check netlist product value
-
-
-    #100;
-    $finish;
-  end
+  #100;
+  $finish;
+end
 
 endmodule
